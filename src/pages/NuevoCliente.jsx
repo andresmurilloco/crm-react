bc460117dd1c1c8d0c4ccb6dd503cc57b7575259
andresmurilloco <andresmurillocortazar@outlook.com>
2023-03-14 06:39:05 -0500

    Validación formulario. Inestable.

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { useNavigate, Form } from 'react-router-dom'
+import { useNavigate, Form, useActionData } from 'react-router-dom'
 import Formulario from '../components/Formulario';
+import Error from '../components/Error'
 
-export function action(){
-  console.log('Hola que hace');
+export async function action({request}){
+  const formData = await request.formData();
+
+  const datos = Object.fromEntries(formData);
+
+  const errores =[];
+  if(Object.values(datos).includes('')){
+    errores.push('Todos los campos son obligatorios')
+  }
+  if(Object.keys(errores).length){
+    return errores;
+  }
 }
 
 const NuevoCliente = () => {
+  const errores = useActionData();
+  console.log(errores);
   const navigate = useNavigate();
 
   return (
@@ -20,6 +33,7 @@ const NuevoCliente = () => {
         </button>
       </div>
       <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        {errores?.length && errores.map((error, i ) => <Error key={i}>{error}</Error>)}
         <Form
           method='post'
         >
